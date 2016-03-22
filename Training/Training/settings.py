4d5bc64193bcc0a995a42f861b17d186cae41d8c
Sunil Kamat <sunilkamatec@gmail.com>
2016-03-22 09:54:12 +0530

    Update settings.py

--- Training/Training/settings.py
+++ Training/Training/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kz=ka0=$^l6%ff^@ai-5rc10-pj1h_*7%=2(r8mprw1#&&()6h'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
